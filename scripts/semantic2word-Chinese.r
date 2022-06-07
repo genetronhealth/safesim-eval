dac3fbb7eff72da34313d2c2482df0c6652418fe
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2022-06-07 19:43:06 +0800

    Added the scripts sematic2word-LANGUAGE.r into this repo where LANGUANGE can be either Chinese or English for now.

---
+++ scripts/semantic2word-Chinese.r
@@ -0,0 +1,51 @@
+#pdf.options(encoding='UTF.8')
+library(showtext)
+showtext_auto()
+#grDevices::cairo_pdf("example.pdf")
+#VAR_mut <- "mut"
+#VAR_AF  <- "AF"
+#VAR_Anno  <- "Anno"
+#VAR_CNT  <- "CNT"
+#VAR_AF_Class  <- "AF_Class"
+#VAR_Simulation <- "Simulation"
+#VAR_TABLE_CNT <- "CNT"
+#VAR_TABLE_Mut <- "Mut"
+
+#VAR_Lbx_low <- "Lbx_low"
+#VAR_Lbx_medium <- "Lbx_medium"
+#VAR_Lbx_high <- "Lbx_high"
+
+#VAR_TABLE_Company <- "Company"
+
+VAR_space_Simulation <- " 模拟"
+VAR_Simulation_space <- "模拟 "
+VAR_Mutation <- "突变"
+VAR_Allele_Fraction <- "等位基因丰度"
+
+VAR_Low <- "低"
+VAR_Medium <- "中"
+VAR_High <- "高"
+
+VAR_Lbx_high_Allele_Fraction_Z_score <- "Lbx_high 等位基因丰度Z值"
+
+VAR_Real_Data_Allele_Fraction_Z_score <- "真实数据的等位基因丰度Z值"
+VAR_Simulation_Allele_Fraction_Z_score <- "模拟变异的等位基因丰度Z值"
+
+VAR_space_QQplot <- " 分位图"
+VAR_QQplot_WES <- "分位图（全外显子测序）"
+
+VAR_Allele_Fraction_Variance <- "(等位基因丰度方差)"
+VAR_space_Variance <- " 方差"
+VAR_Variance <- "方差"
+VAR_Allele_Fraction_Mean <- "(等位基因丰度平均值)"
+VAR_space_Mean <- " 平均值"
+VAR_Mean <- "平均值"
+
+VAR_Simulation_WES <- "模拟结果（全外显子测序）"
+VAR_Value <- "数值"
+VAR_QQplot_WES <- "分位图（全外显子测序）"
+VAR_Variance_WES <- "方差（全外显子测序）"
+VAR_Mean_WES <- "平均值（全外显子测序）"
+
+VAR_RealData_space <- "真实数据 "
+
---
+++ scripts/semantic2word-English.r
@@ -0,0 +1,48 @@
+# source("remote-sensing-functions.R")
+#VAR_mut <- "mut"
+#VAR_AF  <- "AF"
+#VAR_Anno  <- "Anno"
+#VAR_CNT  <- "CNT"
+#VAR_AF_Class  <- "AF_Class"
+#VAR_Simulation <- "Simulation"
+#VAR_TABLE_CNT <- "CNT"
+#VAR_TABLE_Mut <- "Mut"
+
+#VAR_Lbx_low <- "Lbx_low"
+#VAR_Lbx_medium <- "Lbx_medium"
+#VAR_Lbx_high <- "Lbx_high"
+
+#VAR_TABLE_Company <- "Company"
+
+VAR_space_Simulation <- " Simulation"
+VAR_Simulation_space <- "Simulation "
+VAR_Mutation <- "Mutation"
+VAR_Allele_Fraction <- "Allele_Fraction"
+
+VAR_Low <- "Low"
+VAR_Medium <- "Medium"
+VAR_High <- "High"
+
+VAR_Lbx_high_Allele_Fraction_Z_score <- "Lbx_high Allele Fraction Z-Score"
+
+VAR_Real_Data_Allele_Fraction_Z_score <- "RealData Allele Fraction Z-Score"
+VAR_Simulation_Allele_Fraction_Z_score <- "Simulation Allele Fraction Z-Score"
+
+VAR_space_QQplot <- " QQplot"
+VAR_QQplot_WES <- "QQplot (WES)"
+
+VAR_Allele_Fraction_Variance <- "(Allele Fraction Variance)"
+VAR_space_Variance <- " Variance"
+VAR_Variance <- "Variance"
+VAR_Allele_Fraction_Variance <- "(Allele Fraction Mean)"
+VAR_space_Mean <- " Mean"
+VAR_Mean <- "Mean"
+
+VAR_Simulation_WES <- "Simulation (WES)"
+VAR_Value <- "Value"
+VAR_QQplot_WES <- "QQplot (WES)"
+VAR_Variance_WES <- "Variance (WES)"
+VAR_Mean_WES <- "Variance (WES)"
+
+VAR_RealData_space <- "RealData "
+
